2552dc755ef0f5a86d6a1c3aab715f2e175c4cb9
aminopyrodin <aminopyrodin@gmail.com>
2020-07-02 20:59:42 +0500

    add http-request and embedded libraries

--- app/index.js
+++ app/index.js
@@ -1,1 +1,17 @@
-console.log(process.argv[2] + " " + process.argv[3]);
+const got = require('got');
+
+const serverUrl = process.argv[2];
+const playerKey = process.argv[3];
+
+console.log(`ServerUrl: ${serverUrl}; playerKey: ${playerKey}`);
+
+sendRequest();
+
+async function sendRequest() {
+    try {
+        const response = await got(`${serverUrl}?playerKey=${playerKey}`);
+        console.log(response.body);
+    } catch (e) {
+        throw e;
+    }
+}
